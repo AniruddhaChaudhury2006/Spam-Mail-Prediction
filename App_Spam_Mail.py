e7dd5a231df5bffcda045fd68601449a59a765f6
AniruddhaChaudhury2006 <chaudhuryaniruddha56@gmail.com>
2026-03-12 00:39:04 +0530

    Update App_Spam_Mail.py

--- App_Spam_Mail.py
+++ App_Spam_Mail.py
@@ -13,9 +13,9 @@ mail_data.loc[mail_data['Category'] == "ham", 'Category',] = 1
 X = mail_data['Message']
 Y = mail_data['Category']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 3)
-feature_extraction = TfidfVectorizer(min_df = 1, stop_words = 'english', lowercase = True)
-X_train_features = feature_extraction.fit_transform(X_train)
-X_test_features = feature_extraction.transform(X_test)
+vectorizer = TfidfVectorizer(min_df = 1, stop_words = 'english', lowercase = True)
+X_train_features = vectorizer.fit_transform(X_train)
+X_test_features = vectorizer.transform(X_test)
 Y_train = Y_train.astype('int')
 Y_test = Y_test.astype('int')
 model = LogisticRegression()
@@ -60,5 +60,5 @@ if st.button("Detect Spam"):
       idx = list(feature_names).index(word)
       word_scores.append((word, weights[idx]))
   word_scores = sorted(word_scores, key = lambda x: abs(x[1]), reverse = True)[:10]
-  explain_df = pd.DataFrame(word_scores, columns = ["word", "Importance"])
+  explain_df = pd.DataFrame(word_scores, columns = ["Word", "Importance"])
   st.bar_chart(explain_df.set_index("Word"))
